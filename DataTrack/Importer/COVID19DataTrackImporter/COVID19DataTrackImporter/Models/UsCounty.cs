928d4db3f8198d7642284c000441b77e53e43669
Jeffrey Hamlin <sourcesage@gmail.com>
2020-04-05 11:15:23 -0400

    County import table

---
+++ DataTrack/Importer/COVID19DataTrackImporter/COVID19DataTrackImporter/Models/UsCounty.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COVID19DataTrackImporter.Models
+{
+    public class UsCounty
+    {
+        public string date { get; set; }
+        public string county { get; set; }
+        public string state { get; set; }
+        public int? fips { get; set; }
+        public int? cases { get; set; }
+        public int? deaths { get; set; }
+    }
+}
